94be328689f35f20e264550d251b349a143ec77d
jinxing <1085841539@qq.com>
2022-02-20 21:40:48 +0800

    2_7 开头

---
+++ chapter_2/2_7.cpp
@@ -0,0 +1,20 @@
+//
+// Created by ljx on 2022/2/9.
+//
+
+#include <iostream>
+#include <thread>
+#include <list>
+#include <mutex>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    // 一：设计模式大概谈
+    // “设计模式”：代码的一些写法，程序灵活，维护起来方便。
+
+    // 二：单例模式（使用频率高）
+    //
+    return 0;
+}
