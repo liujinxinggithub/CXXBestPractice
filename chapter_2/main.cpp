dae66a4c9ec32e6a1fd3096cb33eb565dfcde813
刘金星 <1085841539@qq.com>
2022-04-20 00:22:50 -0700

    智能指针循环引用例子

--- chapter_2/main.cpp
+++ chapter_2/main.cpp
@@ -8,6 +8,10 @@
 #include <mutex>
 #include <fstream>
 #include <cstring>
+#include <vector>
+#include <future>
+#include <deque>
+#include "../3rdpart/thread-pool/include/ThreadPool.h"
 
 using namespace std;
 
@@ -19,12 +23,14 @@ private:
     std::list<int> msgRecvQueue;
 };
 
-//vector<int> g_v = {1, 2, 3}; //共享数据，只读
+vector<int> g_v = {1, 2, 3}; //共享数据，只读
 //
-//void myprint(int inum) {
-//    cout << "id=" << std::this_thread::get_id << "g_v" << g_v[0] << g_v[1]<< g_v[2] << endl;
-//}
+void myprint(const int & i) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 10秒
+    cout << "id=" << std::this_thread::get_id() << endl;
+    cout << "num=" << i << endl;
 
+}
 class LogFile {
     std::mutex _mu;
     ofstream f;
@@ -35,21 +41,21 @@ public:
     ~LogFile() {
         f.close();
     }
-    void shared_print(const string& msg) {
+    void shared_print(const string &msg) {
         std::unique_lock<std::mutex> guard(_mu);
         cout << "函数中的线程号：" << std::this_thread::get_id() << endl;
     }
 
 };
 
-
 class Test {
 public:
-    Test(int size) : size_(size) {
+    Test(int size)
+        : size_(size) {
         data = new int[size];
     }
-    Test(){}
-    Test(const Test& Test) {
+    Test() {}
+    Test(const Test &Test) {
         size_ = Test.size_;
         data = Test.data;// 浅拷贝
         data = new int[size_]; // 深拷贝
@@ -58,18 +64,23 @@ public:
     ~Test() {
         delete[] data;
     }
+
+    void my_sleep() {
+        std::this_thread::sleep_for(chrono::milliseconds(5*1000));
+    }
+
     int *data;
     int size_;
 };
 
-void thread_test(int& a) {
+void thread_test(int &a) {
     cout << a << endl;
 }
 
 // 图像数据
 struct ImageData {
     typedef std::shared_ptr<ImageData> Ptr;
-    int* buffer; // 图片缓存区
+    int *buffer; // 图片缓存区
 };
 
 int main() {
@@ -88,22 +99,24 @@ int main() {
 //    }
 //    cout << "我爱中国" << endl;
 
-    // 使用成员函数作为线程函数的方法来写线程
+// 使用成员函数作为线程函数的方法来写线程
 //    cout << "主线程号：" << std::hex << std::this_thread::get_id() << endl;
 //    LogFile a;
 //    a.shared_print("hhh");
 
-    char src[40];
-    char dest[100];
+    // char src[40];
+    // char dest[100];
+    //
+    // memset(dest, '\0', sizeof(dest));
+    // strcpy(src, "This is runoob.com");
+    // strcpy(dest, src);
+    //
+    // cout << "&src  " << &src << endl;
+    // cout << "&dest " << &dest << endl;
 
-    memset(dest, '\0', sizeof(dest));
-    strcpy(src, "This is runoob.com");
-    strcpy(dest, src);
-
-    cout << "&src  " << &src << endl;
-    cout << "&dest " << &dest << endl;
-
-
-
+    auto share_ptr = make_shared<int>(0);
+    share_ptr = nullptr;
+    if (!share_ptr)
+        cout << "智能指针 空" << endl;
     return 0;
 }
--- cxx_base/smart_point.cpp
+++ cxx_base/smart_point.cpp
@@ -1,58 +1,45 @@
 #include <iostream>
-#include <list>
 #include <memory>
 
 using namespace std;
 
+class BB;
+class AA {
+public:
+    shared_ptr<BB> b_ptr;
+    ~AA() {
+        cout << "~AA()" << endl;
+    }
+    AA() {
+        cout << "AA()" << endl;
+    }
+};
+
+class BB {
+public:
+    shared_ptr<AA> a_ptr;
+    ~BB() {
+        cout << "~BB()" << endl;
+    }
+    BB() {
+        cout << "BB()" << endl;
+    }
+};
+
 int main() {
-//    shared_ptr<string> p1;
-//    shared_ptr<list<int>> p2;
-//    if(p1  && p1->empty()) {
-//        *p1 = "hi";
-//    }
-//    shared_ptr<int> p3 = make_shared<int>(42);
-//    shared_ptr<string> p4 = make_shared<string>(10,'9');
-//    shared_ptr<int> p5 = make_shared<int>();
-//    auto p = make_shared<int>(42);
-//    cout << "与p共享的智能指针数量" << p.use_count() << endl; //1
-//    cout << "p=" << *p << endl; // 42
-//    cout << "p的地址" << p << endl; // p
-//    auto q(p);
-//    cout << "与q共享的智能指针数量" << q.use_count() << endl; //2
-//    cout << "q=" << *q << endl; // 42
-//    cout << "q的地址" << q << endl; // =p
-//
-//    auto r = make_shared<int>(100);//r指向的int只有一个引用者
-//    cout << "与r共享的智能指针数量" << r.use_count() << endl; //1
-//    cout << "r=" << *r << endl; // 100
-//    cout << "r的地址" << r << endl; //r, !=p
-//    r=q;//给r赋值，令它指向另一个地址
-//    cout << "与r共享的智能指针数量" << r.use_count() << endl; //3
-//    cout << "r=" << *r << endl; // 42
-//    cout << "r的地址" << r << endl; // p
-    //递增q指向的对象的引用计数
-    //递减r原来指向的对象的引用计数
-    //r原来指向的对象已没有引用者，会自动释放
-
-    // shared_ptr<int> p11 = new int(1024);//错误：必须使用直接初始化形式
-    shared_ptr<int> p22(new int(1024));//正确：使用了直接初始化形式
-
-    // p = new int(1024);//错误：不能将一个指针赋予shared_ptr
-//    auto p = make_shared<int>(42);
-//    cout << p.use_count() << endl;
-//    p.reset();//p是唯一指向的对象，reset会释放此对象
-//    cout << p.use_count() << endl;
-//    p.reset(new int(4));//传递了可选的参数内置指针，会让p指向new int(4)
-//    cout << p.use_count() << endl;
-//    auto x = new int(10);
-//    p.reset(x, std::default_delete<int>());//传递了可选的参数内置指针，会让p指向new int(4)
-//    cout << p.use_count() << endl;
-
-    auto p = make_shared<int>(42);
-    cout << p.use_count() << endl;
-
-    weak_ptr<int> w(p);
-    cout << p.use_count() << endl;
+    /*
+ * share_ptr循环引用，引出weak_ptr的作用。
+ * 循环引用：两个对象相互使用shared_ptr成员变量指向对方造成循环引用，导致引用计数失效。
+ * 即A内部有指向B，B内部有指向A; 这样对于AA，必定是在BB析构后才析构; 对于BB，必定是AA析
+ * 构后才析构B;
+ * 这就是循环引用的问题，违反常规，导致内存泄露。
+ */
+    shared_ptr<AA> aa = make_shared<AA>();
+    shared_ptr<BB> bb = make_shared<BB>();
+    aa->b_ptr = bb;
+    bb->a_ptr = aa;
+    cout << aa.use_count() << endl;
+    cout << bb.use_count() << endl;
 
     return 0;
 }
